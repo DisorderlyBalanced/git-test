c0d86663549d8ba780abf0ccf87bc7291458c06a
coco <2383756552@qq.com>
2022-10-23 21:13:29 +0800

    third commit

--- src/main/java/com/coco/git/GItTest.java
+++ src/main/java/com/coco/git/GItTest.java
@@ -1,12 +1,10 @@
 package com.coco.git;
 
-/**
- * @author CC
- * @create 2022-10-23 20:12
- */
+
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
+        System.out.println("hello git3!");
     }
 }

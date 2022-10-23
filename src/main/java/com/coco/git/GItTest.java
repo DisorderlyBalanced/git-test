aa625249412131561613e777257cb5ab72bd1d74
coco <2383756552@qq.com>
2022-10-23 21:12:48 +0800

    second commit

--- src/main/java/com/coco/git/GItTest.java
+++ src/main/java/com/coco/git/GItTest.java
@@ -7,5 +7,6 @@ package com.coco.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }

b33de8b339f0e1491c17dbb9a7277402d4763c2c
coco <2383756552@qq.com>
2022-10-23 20:14:27 +0800

    first commit

---
+++ src/main/java/com/coco/git/GItTest.java
@@ -0,0 +1,11 @@
+package com.coco.git;
+
+/**
+ * @author CC
+ * @create 2022-10-23 20:12
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
